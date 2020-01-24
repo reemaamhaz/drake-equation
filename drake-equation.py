deea9733272c24fd0d12f680e2741c55db3b984d
reemaamhaz <49565663+reemaamhaz@users.noreply.github.com>
2020-01-24 03:49:56 -0800

    Update drake-equation.py

--- drake-equation.py
+++ drake-equation.py
@@ -16,19 +16,40 @@ def travelTime(x):
 def timeConver(t):
     years = t/(24*365)
     return print (years, "years")
-    
+
 y = travelTime(343180)
 timeConver(y)
 
-spaceCraftTable = Table(names=('Name', 'Fastest Speed Recorded (km/h)', 'Farthest Distance Travelled (km)', 'Years in Orbit', 'Year Launched'))
-spaceCraftTable.add_row(('Voyager 1', 62136, 22248009321.487, 42, 1977))
-spaceCraftTable.add_row(('Galileo', 173736, 4600000000, 14, 1989))
-spaceCraftTable.add_row(('Mars Pathfinder', 27000, 497000000, .232, 1997))
-spaceCraftTable.add_row(('Cassini–Huygens', 122000, 7800000000, 20, 1997))
-spaceCraftTable.add_row(('New Horizons', 58536, 6600000000, 14, 2006))
-spaceCraftTable.add_row(('Juno', 209000, 628743036, 8, 2016))
-spaceCraftTable.add_row(('Parker Solar Probe', 343180, 497000000, 2, 2018))
+from astropy.table import Table
 
-spaceCraftTable[:5]
+data_rows = [('Voyager 1', 62136, 22248009321.487, 42, 1977),
+('Galileo', 173736, 4600000000, 14, 1989),
+('Mars Pathfinder', 27000, 497000000, .232, 1997),
+('Cassini–Huygens', 122000, 7800000000, 20, 1997),
+('New Horizons', 58536, 6600000000, 14, 2006),
+('Juno', 209000, 628743036, 8, 2016),
+('Parker Solar Probe', 343180, 497000000, 2, 2018)]
 
-spaceCraftTable.set_index('Year Launched')['Fastest Speed Recorded (km/h)'].plot.line();
+spaceCraftTable = Table(rows=data_rows, names=('Name', 'Fastest Speed Recorded (km/h)', 'Farthest Distance Travelled (km)', 'Years in Orbit', 'Year Launched'))
+
+spaceCraftTable
+
+year = [1977, 1989, 1997, 1997, 2006, 2016, 2018]
+speed = [62136, 173736, 27000, 122000, 58536, 209000, 343180]
+plt.plot(year, speed, color='red')
+plt.xlabel('Year Launched')
+plt.ylabel('Fastest Speed Recorded (km/h)')
+plt.title('Fastest Spacecrafts Launched in the Last 50 Years')
+
+import fbprophet
+data = [['1977-07-23', 62136], ['1989-07-23', 173736], ['1997-07-23', 27000], ['1997-07-23', 122000], ['2006-07-23', 58536], ['2016-07-23', 209000], ['2018-07-23', 343180]]
+df = pd.DataFrame(data, columns = ['ds', 'y']) 
+predictYr = fbprophet.Prophet()
+predictYr.fit(df)
+
+forecast = predictYr.make_future_dataframe(periods=365 * 2, freq='D')
+# Make predictions
+forecast = predictYr.predict(gm_forecast)
+
+predictYr.plot(forecast, xlabel = 'Date', ylabel = 'Fastest Speed (km/h)')
+plt.title('Spacecraft Predictions');
