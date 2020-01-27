d634874590c935ac5e9aeb7e2aa34b0850a769c3
reemaamhaz <49565663+reemaamhaz@users.noreply.github.com>
2020-01-26 22:57:15 -0500

    Update drake-equation.py

--- drake-equation.py
+++ drake-equation.py
@@ -1,15 +1,16 @@
 #import necessary modules and libraries
+#import necessary modules and libraries
 import pandas as pd;
 import numpy as np;
 import matplotlib.pyplot as plt
-import statsmodels.api as sm
-import statsmodels.formula.api as smf
-import matplotlib.pyplot as plt
-import seaborn as sns
+import pandas as pd;
+import plotly.offline as py
+
+from astropy.table import Table
+from fbprophet.plot import plot_plotly
+from fbprophet import Prophet
 
 #import necessary modules and libraries
-from astropy.table import Table
-
 def travelTime(x):
     time = (4.3 * 9500000000000)/ x
     return time
@@ -20,36 +21,39 @@ def timeConver(t):
 y = travelTime(343180)
 timeConver(y)
 
-from astropy.table import Table
-
 data_rows = [('Voyager 1', 62136, 22248009321.487, 42, 1977),
 ('Galileo', 173736, 4600000000, 14, 1989),
-('Mars Pathfinder', 27000, 497000000, .232, 1997),
+('Mars Pathfinder', 27000, 497000000, .232, 1996),
 ('Cassini–Huygens', 122000, 7800000000, 20, 1997),
 ('New Horizons', 58536, 6600000000, 14, 2006),
-('Juno', 209000, 628743036, 8, 2016),
+('Juno', 209000, 628743036, 8, 2011),
 ('Parker Solar Probe', 343180, 497000000, 2, 2018)]
 
-spaceCraftTable = Table(rows=data_rows, names=('Name', 'Fastest Speed Recorded (km/h)', 'Farthest Distance Travelled (km)', 'Years in Orbit', 'Year Launched'))
+spaceCraftTable = Table(rows=data_rows, names=('Name', 'Fastest Speed Recorded (km/h)', 'Farthest Distance Travelled (km)', 'Years in Orbit', 'Date Launched'))
 
 spaceCraftTable
 
-year = [1977, 1989, 1997, 1997, 2006, 2016, 2018]
+year = [1977, 1989, 1996, 1997, 2006, 2011, 2018]
 speed = [62136, 173736, 27000, 122000, 58536, 209000, 343180]
 plt.plot(year, speed, color='red')
 plt.xlabel('Year Launched')
 plt.ylabel('Fastest Speed Recorded (km/h)')
 plt.title('Fastest Spacecrafts Launched in the Last 50 Years')
 
-import fbprophet
-data = [['1977-07-23', 62136], ['1989-07-23', 173736], ['1997-07-23', 27000], ['1997-07-23', 122000], ['2006-07-23', 58536], ['2016-07-23', 209000], ['2018-07-23', 343180]]
+
+data = [['1977-09-05', 62136], ['1989-10-18', 173736], ['2011-08-05', 209000], ['2018-08-12', 343180]]
 df = pd.DataFrame(data, columns = ['ds', 'y']) 
-predictYr = fbprophet.Prophet()
-predictYr.fit(df)
+df['cap'] = 50000000
 
-forecast = predictYr.make_future_dataframe(periods=365 * 2, freq='D')
-# Make predictions
-forecast = predictYr.predict(gm_forecast)
+m = Prophet(growth='logistic')
+m.fit(df)
+future = m.make_future_dataframe(periods=365*240)
+future['cap'] = 50000000
+fcst = m.predict(future)
 
-predictYr.plot(forecast, xlabel = 'Date', ylabel = 'Fastest Speed (km/h)')
-plt.title('Spacecraft Predictions');
+y = travelTime(50034460)
+timeConver(y)
+
+py.init_notebook_mode()
+fig = plot_plotly(m, fcst)  # This returns a plotly Figure
+py.iplot(fig)
